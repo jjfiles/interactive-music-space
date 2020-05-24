3072a43b47f41f7e15fe658059d85e747ca57b5a
Jeffery <jeffery@jfiles.dev>
2020-05-24 15:45:19 -0500

    added SteamVR, edited CameraRig prefab to hold all 4 controllers

---
+++ IMS/Assets/SteamVR/Scripts/ViveInput.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class ViveInput : MonoBehaviour
+{
+    public SteamVR_TrackedObject mTrackedObject = null;
+    //public SteamVR
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

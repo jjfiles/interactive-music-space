d1a850536514c2d2404a5fb195fb315efefe56eb
Jeffery <jeffery@jfiles.dev>
2020-06-12 15:57:45 -0500

    rearranged fmod events. Pre-load bank and set zones to play on mouseDown

---
+++ IMS/Assets/Scripts/audioTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audioTest : MonoBehaviour {
+    
+    [FMODUnity.EventRef]
+    public string selectSound;
+    [FMODUnity.BankRef]
+    public string selectBank;
+    FMOD.Studio.EventInstance soundEvent;
+
+    public KeyCode pressToPlaySound;
+
+    void Start() {
+        FMODUnity.RuntimeManager.HasBankLoaded("bank:/Master");
+        soundEvent = FMODUnity.RuntimeManager.CreateInstance(selectSound);
+    }
+
+    void Update() {
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(soundEvent, GetComponent<Transform>(), GetComponent<Rigidbody>());
+    }
+
+    void playSound() {
+        if (Input.GetKeyDown(pressToPlaySound)) {
+            soundEvent.start();
+        }
+    }
+}
--- test.cs
+++ test.cs
@@ -1,7 +1,26 @@
-/*
-TODO:
-- physical line render
-- spatializers?
-*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
- 
+public class test : MonoBehaviour {
+    
+    [FMODUnity.EventRef]
+    public string selectSound;
+    FMOD.Studio.EventInstance soundEvent;
+
+    public KeyCode pressToPlaySound;
+
+    void Start() {
+        soundEvent = FMODUnity.RuntimeManager.CreateInstance(selectSound);
+    }
+
+    void Update() {
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(soundEvent, GetComponent<Transform>(), GetComponent<Rigidbody>());
+    }
+
+    void playSound() {
+        if (Input.GetKeyDown(pressToPlaySound)) {
+            soundEvent.start();
+        }
+    }
+}

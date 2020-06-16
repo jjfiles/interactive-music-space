16c15d636bcb62c61eb221fca0189764fd8879a6
Jeffery <jeffery@jfiles.dev>
2020-06-16 16:13:45 -0500

    fixed fmod corruption

--- IMS/Assets/Scripts/paramTest.cs
+++ IMS/Assets/Scripts/paramTest.cs
@@ -11,14 +11,7 @@ public class paramTest : MonoBehaviour
     FMOD.Studio.EventInstance soundEvent;
     public KeyCode play;
     public KeyCode stop;
-    public KeyCode vol1keyU;
-    public KeyCode vol1keyD;
-    public KeyCode vol2keyU;
-    public KeyCode vol2keyD;
-    public KeyCode vol3keyU;
-    public KeyCode vol3keyD;
-    public KeyCode vol4keyU;
-    public KeyCode vol4keyD;
+
 
     // Start is called before the first frame update
     void Start()
@@ -72,30 +65,6 @@ public class paramTest : MonoBehaviour
         }
         if (Input.GetKeyDown(stop)) {
             soundEvent.stop(0);
-        }
-        if (Input.GetKeyDown(vol1keyU)) {
-            soundEvent.setParameterByName(zoneNum + "-1", 1);
-        }
-        if (Input.GetKeyDown(vol1keyD)) {
-            soundEvent.setParameterByName(zoneNum + "-1", 0);
-        }
-        if (Input.GetKeyDown(vol2keyU)) {
-            soundEvent.setParameterByName(zoneNum + "-2", 1);
-        }
-        if (Input.GetKeyDown(vol2keyD)) {
-            soundEvent.setParameterByName(zoneNum + "-2", 0);
-        }
-        if (Input.GetKeyDown(vol3keyU)) {
-            soundEvent.setParameterByName(zoneNum + "-3", 1);
-        }
-        if (Input.GetKeyDown(vol3keyD)) {
-            soundEvent.setParameterByName(zoneNum + "-3", 0);
-        }
-        if (Input.GetKeyDown(vol4keyU)) {
-            soundEvent.setParameterByName(zoneNum + "-4", 1);
-        }
-        if (Input.GetKeyDown(vol4keyD)) {
-            soundEvent.setParameterByName(zoneNum + "-4", 0);
         }
         
     }

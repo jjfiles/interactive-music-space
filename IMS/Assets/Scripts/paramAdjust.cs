c2e20dc547ba1c30530713ddb2c7d4019e4703a2
Jeffery Files <jeffery@jfiles.dev>
2020-09-01 13:01:51 -0500

    moved fmod to 1st mix

--- IMS/Assets/Scripts/paramAdjust.cs
+++ IMS/Assets/Scripts/paramAdjust.cs
@@ -24,39 +24,37 @@ public class paramAdjust : MonoBehaviour
 //? conditional for height vairance controlling some param i.e. alt or reverb
     void Update() 
     {
-        print("update");
         FMODUnity.RuntimeManager.AttachInstanceToGameObject(sEvent, GetComponent<Transform>(), GetComponent<Rigidbody>());
         controls(); 
     }
 
     private void OnTriggerEnter(Collider other) 
     {
-        if (!other.name.Contains("z")) {
+        if (!other.name.Contains("z"))
+        {
             sEvent.setParameterByName(zone + other.name, 1);
-            print("paramter: " + zone + other.name + " on");
         }
     }
 
     private void OnTriggerExit(Collider other) 
     {
-        if (!other.name.Contains("z")) {
+        if (!other.name.Contains("z"))
+        {
             sEvent.setParameterByName(zone + other.name, 0);
-            print("paramter: " + zone + other.name + " off");
         }
     }
 
     void controls() {
-        print("controls called");
         if (Input.GetKeyDown(playbackStart))
         {
             sEvent.start();
-            print("Start called");
         }
+
         if (Input.GetKeyDown(playbackStop))
         {
             sEvent.stop(0);
-            print("Stop Called");
         }
+
         if (Input.GetKeyDown(playbackMute))
         {
             for (int i = 1; i < 5; i++)

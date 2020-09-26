f8bfd93a31dd5cf80aad9242dcb95bb966bbe45b
Jeffery Files <jeffery@jfiles.dev>
2020-09-25 21:48:30 -0500

    updated tracks and FMOD build. Moved to ver 1.1

--- IMS/Assets/Scripts/roomSize.cs
+++ IMS/Assets/Scripts/roomSize.cs
@@ -13,10 +13,6 @@ public class roomSize : MonoBehaviour
         var chap = OpenVR.Chaperone;
         bool checkPlayArea = (chap != null) && chap.GetPlayAreaSize(ref pX, ref pY);
 
-        print("x: " + pX);
-        print("y: " + pY);
-        
-
         gameObject.transform.localScale = new Vector3((pX/4)-.1f, 1, (pY/4)-.2f);
     }
 
